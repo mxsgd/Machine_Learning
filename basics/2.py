19f3817310dd920b1c0dfe92e3e8f9b0be3dfe7f
mxsgd <polknos@gmail.com>
2023-12-01 23:28:23 +0100

    update

--- basics/2.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.arange(5)
-y = (9-4)*x**2+(3-5)*x+(3-6)
-g = (np.e**x)/(np.e**x+1)
-plt.plot(x, y, 'g')
-plt.plot(x, g, 'r')
-plt.show()
-print(np.e)
